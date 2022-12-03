69a9de9d678ae32b2a2bc61cf894a0fce7cbfcb2
Dmitriy Bubyr <d.bubyr@lancktele.com>
2022-12-03 16:59:37 +0300

    Fix after code review

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -6,70 +6,65 @@ import ru.netology.model.Post;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 // Stub
 @Repository
 public class PostRepository {
-  private final CopyOnWriteArrayList<Post> posts = new CopyOnWriteArrayList<>();
-  private AtomicInteger counterId = new AtomicInteger(0);
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong counterId = new AtomicLong(0);
 
-  private List<Post> getActualPosts() {
-    return posts.stream().filter(it -> !it.isRemoved()).collect(Collectors.toList());
-  }
-
-  public List<Post> all() {
-    return getActualPosts();
-  }
-
-  public Optional<Post> getById(long id) throws NotFoundException {
-    if (!posts.isEmpty()) {
-      for (int i = 0; i < posts.size(); i++) {
-        var curPost = posts.get(i);
-        if (curPost.getId() == id) {
-          if (curPost.isRemoved()) {
-            throw new NotFoundException();
-          }
-          return Optional.of(curPost);
-        }
-      }
+    private List<Post> getActualPosts() {
+        return posts.values().stream().filter(it -> !it.isRemoved()).collect(Collectors.toList());
     }
-    return Optional.empty();
-  }
 
-  public Post save(Post post) throws NotFoundException {
-    int newPostId = 0;
-    if (post.getId() == newPostId) {
-      counterId.incrementAndGet();
-      post.setId(counterId.get());
-      posts.add(post);
-    } else {
-      for (var curPost: posts) {
-        if (curPost.getId() == post.getId()) {
-          if (curPost.isRemoved()) {
-            throw new NotFoundException();
-          }
-          curPost.setContent(post.getContent());
-          return post;
-        }
-      }
-      post.setId(posts.size());
-      posts.add(post);
+    public List<Post> all() {
+        return getActualPosts();
     }
-    return post;
-  }
 
-  public void removeById(long id) {
-    var actualPosts = getActualPosts();
-    if (!actualPosts.isEmpty()) {
-      for (int i = 0; i < actualPosts.size(); i++) {
-        if (actualPosts.get(i).getId() == id) {
-          actualPosts.get(i).setRemoved(true);
-          return;
+    public Optional<Post> getById(long id) throws NotFoundException {
+        if (!posts.isEmpty() && posts.containsKey(id)) {
+            Post foundPost = posts.get(id);
+            if (foundPost.isRemoved()) {
+                throw new NotFoundException();
+            } else {
+                return Optional.of(foundPost);
+            }
         }
-      }
+        return Optional.empty();
     }
-  }
+
+    public Post save(Post post) throws NotFoundException {
+        int newPostId = 0;
+        if (post.getId() == newPostId) {
+            post.setId(counterId.incrementAndGet());
+            posts.put(post.getId(), post);
+        } else {
+            Post postToUpdate = posts.get(post.getId());
+            if (postToUpdate != null) {
+                if (postToUpdate.isRemoved()) {
+                    throw new NotFoundException();
+                } else {
+                    posts.replace(post.getId(), post);
+                }
+            } else {
+                post.setId(counterId.incrementAndGet());
+                posts.put(post.getId(), post);
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        List<Post> actualPosts = getActualPosts();
+        if (!actualPosts.isEmpty()) {
+            actualPosts.stream()
+                .filter(it -> it.getId() == id)
+                .collect(Collectors.toList())
+                .get(0)
+                .setRemoved(true);
+        }
+    }
 }
